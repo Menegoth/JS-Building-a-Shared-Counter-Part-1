6b50d7669d4201fd238e765dc93c4cbcf697b892
Menegoth <antonioareas@me.com>
2022-07-02 10:40:32 -0700

    Assignment complete

--- public/index.js
+++ public/index.js
@@ -1,9 +1,12 @@
-function main(){
+async function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
+    const refreshButton = document.querySelector("#refresh-button");
 
-    let countValue = 0;
+    let response = await fetch("http://localhost:9001/counter");
+    let result = await response.json();
+    let countValue = result.value;
 
     function increment(){
         countValue++;
@@ -15,8 +18,14 @@ function main(){
         countContainer.textContent = countValue;
     }
 
+    function refresh() {
+        countValue = result.value;
+        countContainer.textContent = result.value;
+    }
+
     incrementButton.addEventListener('click', increment);
     decrementButton.addEventListener('click', decrement);
+    refreshButton.addEventListener("click", refresh);
     countContainer.textContent = countValue;
 }
 main()
